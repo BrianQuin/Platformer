a7d61760495374aae547de4d245981071c8d7114
Luissolis2020 <luissolis428@gmail.com>
2023-04-26 20:23:15 -0500

    Added a Main Menu and a new Button class and some assets to help the main menu and the rest of the game

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 import random
 from pygame.locals import *
+from Button import Button
 
 pygame.init()
 vec = pygame.math.Vector2   #Two Demenstion 
@@ -17,6 +18,8 @@ FramePerSecond = pygame.time.Clock()
 window = pygame.display.set_mode((screenWidth, screenHeight))
 pygame.display.set_caption("2D Game")
 
+def get_font(size): # Returns Press-Start-2P in the desired size
+    return pygame.font.Font("assets/font.ttf", size)
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
@@ -84,37 +87,77 @@ for x in range(random.randint(5, 6)):
     platforms.add(pl)
     all_sprites.add(pl)
 
-while True:
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                P1.jump()
+   
+def play():
 
-    window.fill((0,0,0))
-    P1.update()
+    while True:
 
-    for entity in all_sprites:
-        window.blit(entity.surf, entity.rect)
-        entity.move()       
+        window.fill("black")
 
-    if P1.rect.right <= screenWidth / 5:
-        P1.pos.x += abs(P1.vel.x)
-        for plat in platforms:
-            plat.rect.x += abs(P1.vel.x)
-            PT1.surf = pygame.Surface((screenWidth, 20))
-            PT1.surf.fill((255,0,0))
-            PT1.rect = PT1.surf.get_rect(center = (screenWidth/2, screenHeight - 10))
+        for event in pygame.event.get():
+            if event.type == QUIT:
+             pygame.quit()
+             sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    P1.jump()
 
-    if P1.rect.left >= screenWidth / 3:
-        P1.pos.x -= abs(P1.vel.x)
-        for plat in platforms:
-            plat.rect.x -= abs(P1.vel.x)
-            PT1.surf = pygame.Surface((screenWidth, 20))
-            PT1.surf.fill((255,0,0))
-            PT1.rect = PT1.surf.get_rect(center = (screenWidth/2, screenHeight - 10))
+        window.fill((0,0,0))
+        P1.update()
 
-    pygame.display.update()
-    FramePerSecond.tick(FPS)
+        for entity in all_sprites:
+             window.blit(entity.surf, entity.rect)
+             entity.move()       
+
+        if P1.rect.right <= screenWidth / 5:
+            P1.pos.x += abs(P1.vel.x)
+            for plat in platforms:
+                plat.rect.x += abs(P1.vel.x)
+                PT1.surf = pygame.Surface((screenWidth, 20))
+                PT1.surf.fill((255,0,0))
+                PT1.rect = PT1.surf.get_rect(center = (screenWidth/2, screenHeight - 10))
+
+        if P1.rect.left >= screenWidth / 3:
+            P1.pos.x -= abs(P1.vel.x)
+            for plat in platforms:
+                plat.rect.x -= abs(P1.vel.x)
+                PT1.surf = pygame.Surface((screenWidth, 20))
+                PT1.surf.fill((255,0,0))
+                PT1.rect = PT1.surf.get_rect(center = (screenWidth/2, screenHeight - 10))
+
+        pygame.display.update()
+        FramePerSecond.tick(FPS)
+
+def main_menu():
+
+    while True:   
+
+        MENU_MOUSE_POS = pygame.mouse.get_pos()
+
+        MENU_TEXT = get_font(80).render("MAIN MENU", True, "#b68f40")
+        MENU_RECT = MENU_TEXT.get_rect(center=(450, 100))
+
+        PLAY_BUTTON = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(450, 250), 
+                            text_input="PLAY", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+        QUIT_BUTTON = Button(image=pygame.image.load("assets/Quit Rect.png"), pos=(450, 450), 
+                            text_input="QUIT", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+
+        window.blit(MENU_TEXT, MENU_RECT)
+
+        for button in [PLAY_BUTTON, QUIT_BUTTON]:
+            button.changeColor(MENU_MOUSE_POS)
+            button.update(window)
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if PLAY_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    play()
+                if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    pygame.quit()
+                    sys.exit()
+
+        pygame.display.update()
+main_menu()     
